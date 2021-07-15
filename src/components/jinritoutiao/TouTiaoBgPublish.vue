b10fde20b8e041e8b9757e9cf0ab7c049a00cb92
D.Boy.Blade <ykw0104@163.com>
2021-07-15 14:43:37 +0800

    头条

--- src/components/jinritoutiao/TouTiaoBgPublish.vue
+++ src/components/jinritoutiao/TouTiaoBgPublish.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- 添加和修改文章共用改组件 -->
+  <!-- 添加和修改文章共用该组件 -->
   <div class="publish-container">
     <el-card class="box-card box1">
       <template #header>
@@ -14,14 +14,17 @@
       <!-- 1.2 文章发布 开始 -->
       <el-form ref="articleForm"
                :model="article"
+               :rules="articleRules"
                label-width="50px">
 
-        <el-form-item label="标题">
+        <el-form-item label="标题"
+                      prop="title">
           <el-input v-model="article.title"
                     style="width:500px;"></el-input>
         </el-form-item>
 
-        <el-form-item label="内容">
+        <el-form-item label="内容"
+                      prop="content">
           <el-input type="textarea"
                     v-model="article.content"></el-input>
         </el-form-item>
@@ -35,7 +38,8 @@
           </el-radio-group>
         </el-form-item>
 
-        <el-form-item label="频道">
+        <el-form-item label="频道"
+                      prop="channel_id">
           <el-select v-model="article.channel_id"
                      placeholder="请选择频道">
             <el-option v-for="channel in channels"
@@ -80,12 +84,29 @@ export default {
       },
 
       channels: [], // 文章频道
+
+      // 表单验证
+      articleRules: {
+        title: [
+          { required: true, message: '请输入文章标题', trigger: 'blur' },
+          {
+            min: 5,
+            max: 30,
+            message: '长度在 5 到 30 个字符',
+            trigger: 'blur',
+          },
+        ],
+        content: [
+          { required: true, message: '请输入文章内容', trigger: 'blur' },
+        ],
+        channel_id: [{ required: true, message: '请选择文章频道' }],
+      },
     }
   },
   created() {
     this.loadChannels()
 
-    // 有Id,说明是编辑页跳转过来的
+    // 有Id,说明是内容管理页点击编辑按钮跳转过来的
     if (this.$route.query.id) {
       this.loadArticle() // 加载文章内容
     }
@@ -100,29 +121,36 @@ export default {
 
     /* 发布(添加/更新)文章 */
     onPublish(draft = false) {
-      const articleId = this.$route.query.id
+      this.$refs['articleForm'].validate((valid) => {
+        if (!valid) {
+          return // 表单验证失败
+        }
 
-      if (articleId) {
-        // 更新文章
-        updateArticle(articleId, this.article, draft).then((res) => {
-          this.$message({
-            message: `${draft ? '更新并存入草稿' : '更新并发布'}`,
-            type: 'success',
+        // 提交表单
+        const articleId = this.$route.query.id
+
+        if (articleId) {
+          // 更新文章
+          updateArticle(articleId, this.article, draft).then((res) => {
+            this.$message({
+              message: `${draft ? '更新并存入草稿' : '更新并发布'}`,
+              type: 'success',
+            })
+
+            this.$router.push('/toutiao/tt_bg/bg_article') // 更新完后跳转到内容管理页面
           })
+        } else {
+          // 添加新文章
+          addArticle(this.article, draft).then((res) => {
+            this.$message({
+              message: `${draft ? '已存入草稿' : '已发布'}`,
+              type: 'success',
+            })
 
-          this.$router.push('/toutiao/tt_bg/bg_article') // 更新完后跳转到内容管理页面
-        })
-      } else {
-        // 添加新文章
-        addArticle(this.article, draft).then((res) => {
-          this.$message({
-            message: `${draft ? '已存入草稿' : '已发布'}`,
-            type: 'success',
+            this.$router.push('/toutiao/tt_bg/bg_article') // 添加完后跳转到内容管理页面
           })
-
-          this.$router.push('/toutiao/tt_bg/bg_article') // 添加完后跳转到内容管理页面
-        })
-      }
+        }
+      })
     },
 
     /* 加载文章内容 */
